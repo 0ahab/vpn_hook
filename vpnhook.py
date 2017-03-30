86a465d5bd42ac744635e01e24c9c73e14641623
Eric Kloster <ekloster@akamai.com>
2017-03-30 13:57:18 -0400

    Fixing messaging.

--- vpnhook.py
+++ vpnhook.py
@@ -6,7 +6,8 @@ class VPNHook(BotPlugin):
 	"""Errbot plugin to run reverse shell"""
 	@webhook('/vpnhook/<name>/')
 	def vpnhook(self, request, name):
-		send_message(self.build_identifier('#ops'), "User: "+ name+ " logged into VPN")
+		self.send(self.build_identifier('<@ops>'), "User: "+ name+ " logged into VPN")
 		return("User logged in.")
 
 # bang
+
