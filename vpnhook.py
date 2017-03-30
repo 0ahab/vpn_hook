f866a0514aa2865bbfdb375342337aaf456d6d66
Eric Kloster <ekloster@akamai.com>
2017-03-30 11:49:44 -0400

    Fixing messaging.

--- vpnhook.py
+++ vpnhook.py
@@ -6,9 +6,6 @@ class VPNHook(BotPlugin):
 	"""Errbot plugin to run reverse shell"""
 	@webhook('/vpnhook/<name>/')
 	def vpnhook(self, request, name):
-        identifier = self.build_identifier('#ops')
-        self.send_card(title='Title + Body',
-			body=name + ' has logged into the VPN.',
-			thumbnail='https://raw.githubusercontent.com/errbotio/errbot/master/docs/_static/errbot.png',
-			image='https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png',
-			color='red')
+        identifier = self.build_identifier('#ops/casper')
+        self.send(identifier, "%s logged into VPN" % name,
+)
